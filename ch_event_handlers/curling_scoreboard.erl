30db5d29abcf70b8fb190a70e7f51732767e8ebd
Navneet Gupta <navneet_gupta@tutamail.com>
2019-01-17 10:22:29 +0400

    changes

---
+++ ch_event_handlers/curling_scoreboard.erl
@@ -0,0 +1,36 @@
+-module(curling_scoreboard).
+-author("Navneet Gupta").
+-behaviour(gen_event).
+-import(curling_scoreboard_hw, [set_teams/2, next_round/0, add_point/1, reset_board/0]).
+-export([init/1, handle_event/2, handle_call/2, handle_info/2, code_change/3,
+terminate/2]).
+
+init([]) ->
+  {ok, []}.
+
+handle_event({set_team, TeamA, TeamB}, State) ->
+  curling_scoreboard_hw:set_teams(TeamA, TeamB),
+  {ok, State};
+
+handle_event({add_points, Team, N}, State) ->
+  [curling_scoreboard_hw:add_point(Team) || _ <- lists:seq(1, N)],
+  {ok, State};
+
+handle_event(next_round, State) ->
+  curling_scoreboard_hw:next_round(),
+  {ok, State};
+
+handle_event(_, State) ->
+  {ok, State}.
+
+handle_call(_, State) ->
+  {ok, ok, State}.
+
+handle_info(_, State) ->
+  {ok, State}.
+
+code_change(_OldVsn, State, _Extra) ->
+  {ok, State}.
+
+terminate(_Reason, _State) ->
+  ok.
