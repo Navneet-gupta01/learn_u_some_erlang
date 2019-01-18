6f2f0f45b848c02ea59960e32b1c8c6d68645291
Navneet Gupta <navneet_gupta@tutamail.com>
2019-01-18 17:21:55 +0400

    changes

--- ch_event_handlers/curling.erl
+++ ch_event_handlers/curling.erl
@@ -1,6 +1,6 @@
 -module(curling).
 -author("Navneet Gupta").
--export([start_link/2, add_points/3, next_round/1, set_teams/3]).
+-export([start_link/2, add_points/3, next_round/1, set_teams/3, join_feed/2, leave_feed/2]).
 -import(curling_scoreboard, [init/1, handle_event/2, handle_call/2, handle_info/2, code_change/3,
 terminate/2]).
 
@@ -24,6 +24,13 @@ set_teams(Pid, TeamA, TeamB) ->
 send_notification(Pid, Msg) ->
   gen_event:notify(Pid, Msg).
 
+join_feed(Pid, ToPid) ->
+  HandlerId = {curling_feed, make_ref()},
+  gen_event:add_handler(Pid, HandlerId, [ToPid]),
+  HandlerId.
+
+leave_feed(Pid, HandlerId) ->
+  gen_event:delete_handler(Pid, HandlerId, leave_feed).
 
 % c(curling_scoreboard_hw).
 % c(curling_scoreboard).
