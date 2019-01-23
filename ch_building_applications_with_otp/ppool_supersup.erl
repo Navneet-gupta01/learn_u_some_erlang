eddd623195023a718917192bc797698c2696939d
Navneet Gupta <navneet_gupta@tutamail.com>
2019-01-23 11:38:52 +0400

    changes

--- ch_building_applications_with_otp/ppool_supersup.erl
+++ ch_building_applications_with_otp/ppool_supersup.erl
@@ -18,8 +18,14 @@ init([]) ->
   MaxTime = 3600,
   {ok, {{one_for_one, MaxRestart, MaxTime}, []}}.
 
-start_pool(_A,_B,_C) ->
-  io:format("Starting Pool~n",[]).
+start_pool(Name, Limit, MFA) ->
+  io:format("Starting Pool~n",[]),
+  ChildSpec = {Name,
+                {ppool_sup, start_link, [Name, Limit, MFA]},
+                  permanent, 10500, supervisor, [ppool_sup]},
+  supervisor:start_child(ppool, ChildSpec).
 
-stop_pool(A) ->
-  io:format("Stoping Pool ~s~n",A).
+stop_pool(Name) ->
+  io:format("Stoping Pool ~s~n", Name),
+  supervisor:terminate_child(ppool, Name),
+  supervisor:delete_child(ppool, Name).
